61060a31105c4b0ce464b6c1002c7b1efa77064b
milealaur99 <165919184+milealaur99@users.noreply.github.com>
2024-10-16 23:50:02 +0300

    fix

--- frontend/event-management/src/util/getCsrfToken.ts
+++ frontend/event-management/src/util/getCsrfToken.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+axios.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
 
 export const getCsrfToken = async () => {
   const response = await axios.get(
     `${process.env.REACT_APP_BACKEND_URL}/api/csrf-token`
   );
-  axios.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
+
   axios.defaults.headers.post["X-CSRF-Token"] = response.data.csrfToken;
   axios.defaults.headers.put["X-CSRF-Token"] = response.data.csrfToken;
   axios.defaults.headers.patch["X-CSRF-Token"] = response.data.csrfToken;
