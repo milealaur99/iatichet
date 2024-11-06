a1be93e53ae86b84b598bc6a5443abeda0757864
milealaur99 <165919184+milealaur99@users.noreply.github.com>
2024-11-06 15:09:54 +0200

    inspect pdfGen

--- backend/src/utils/pdfGenerator.ts
+++ backend/src/utils/pdfGenerator.ts
@@ -12,119 +12,135 @@ export const generateReservationPDF = async (
   reservation: Reservation,
   path: string
 ) => {
-  const doc = new PDFDocument({
-    size: "A4",
-    layout: "portrait",
-    margins: {
-      top: 50,
-      bottom: 50,
-      left: 50,
-      right: 50
+  try {
+    const doc = new PDFDocument({
+      size: "A4",
+      layout: "portrait",
+      margins: {
+        top: 50,
+        bottom: 50,
+        left: 50,
+        right: 50,
+      },
+    });
+    console.log(doc);
+    doc.pipe(fs.createWriteStream(path));
+
+    const user = await User.findById(reservation.user);
+    if (!user) {
+      throw new AppError("User not found", 404);
     }
-  });
+    console.log(user);
+    const event = (await EventModel.findById(reservation.event).populate(
+      "hall"
+    )) as EventType & { hall: { name: string } };
+    console.log(event);
+    if (!event) {
+      throw new AppError("Event not found", 404);
+    }
 
-  doc.pipe(fs.createWriteStream(path));
-
-  const user = await User.findById(reservation.user);
-  if (!user) {
-    throw new AppError("User not found", 404);
-  }
-
-  const event = (await EventModel.findById(reservation.event).populate(
-    "hall"
-  )) as EventType & { hall: { name: string } };
-
-  if (!event) {
-    throw new AppError("Event not found", 404);
-  }
-
-  const seatsPerPage = 6;
-  const logoImage = fs.readFileSync(
-    pathFunction.join(
-      __dirname,
-      "..",
-      "..",
-      "assets",
-      "default_transparent_765x625.png"
-    )
-  );
-  const addHeader = () => {
-    doc
-      .fillColor("#333333")
-      .fontSize(26)
-      .font("Helvetica-Bold")
-      .image(logoImage, { align: "center", width: 150 })
-      .moveDown(0.5)
-      .fontSize(12)
-      .font("Helvetica")
-      .fillColor("#555555")
-      .text(`Date: ${new Date().toLocaleDateString()}`, { align: "right" })
-      .moveDown(1.5)
-      .fontSize(20)
-      .fillColor("#0077cc")
-      .text(event.name, { align: "center" })
-      .moveDown(0.5)
-      .fontSize(14)
-      .fillColor("#666666")
-      .text(`Date: ${new Date(event.date).toLocaleDateString()}`, {
-        align: "center"
-      })
-      .text(`Hall: ${event.hall.name}`, { align: "center" })
-      .moveDown(1);
-  };
-
-  const addFooter = () => {
-    doc
-      .fontSize(10)
-      .fillColor("#999999")
-      .text("For any questions, please contact the event organizer.", 50, 750, {
-        align: "center",
-        width: 500
-      })
-      .moveDown(0.5)
-      .fontSize(12)
-      .font("Helvetica-Bold")
-      .fillColor("#444444")
-      .text("Thank you for your reservation!", { align: "center", width: 500 })
-      .fontSize(10)
-      .font("Helvetica")
-      .fillColor("#aaaaaa")
-      .text(`Page ${doc.bufferedPageRange().count}`, 50, 780, {
-        align: "center"
-      });
-  };
-
-  const addSeats = (seats: Seat[]) => {
-    seats.forEach((seat, index) => {
-      const qrImage = qr.imageSync(
-        `${reservation._id}-${seat.row}-${seat.number}`,
-        { type: "png" }
-      );
-
+    const seatsPerPage = 6;
+    const logoImage = fs.readFileSync(
+      pathFunction.join(
+        __dirname,
+        "..",
+        "..",
+        "assets",
+        "default_transparent_765x625.png"
+      )
+    );
+    const addHeader = () => {
       doc
-        .moveDown(1)
-        .image(qrImage, { width: 80, align: "center" })
+        .fillColor("#333333")
+        .fontSize(26)
+        .font("Helvetica-Bold")
+        .image(logoImage, { align: "center", width: 150 })
+        .moveDown(0.5)
+        .fontSize(12)
+        .font("Helvetica")
+        .fillColor("#555555")
+        .text(`Date: ${new Date().toLocaleDateString()}`, { align: "right" })
+        .moveDown(1.5)
+        .fontSize(20)
+        .fillColor("#0077cc")
+        .text(event.name, { align: "center" })
         .moveDown(0.5)
         .fontSize(14)
-        .font("Helvetica-Bold")
-        .fillColor("#333333")
-        .text(`Row: ${seat.row}, Seat: ${seat.number}`, {
-          align: "center"
+        .fillColor("#666666")
+        .text(`Date: ${new Date(event.date).toLocaleDateString()}`, {
+          align: "center",
         })
-        .moveDown(3);
-    });
-  };
+        .text(`Hall: ${event.hall.name}`, { align: "center" })
+        .moveDown(1);
+    };
 
-  addHeader();
-  addSeats(reservation.seats.slice(0, seatsPerPage));
-  addFooter();
+    const addFooter = () => {
+      doc
+        .fontSize(10)
+        .fillColor("#999999")
+        .text(
+          "For any questions, please contact the event organizer.",
+          50,
+          750,
+          {
+            align: "center",
+            width: 500,
+          }
+        )
+        .moveDown(0.5)
+        .fontSize(12)
+        .font("Helvetica-Bold")
+        .fillColor("#444444")
+        .text("Thank you for your reservation!", {
+          align: "center",
+          width: 500,
+        })
+        .fontSize(10)
+        .font("Helvetica")
+        .fillColor("#aaaaaa")
+        .text(`Page ${doc.bufferedPageRange().count}`, 50, 780, {
+          align: "center",
+        });
+    };
 
-  for (let i = seatsPerPage; i < reservation.seats.length; i += seatsPerPage) {
-    doc.addPage();
+    const addSeats = (seats: Seat[]) => {
+      seats.forEach((seat, index) => {
+        const qrImage = qr.imageSync(
+          `${reservation._id}-${seat.row}-${seat.number}`,
+          { type: "png" }
+        );
+
+        doc
+          .moveDown(1)
+          .image(qrImage, { width: 80, align: "center" })
+          .moveDown(0.5)
+          .fontSize(14)
+          .font("Helvetica-Bold")
+          .fillColor("#333333")
+          .text(`Row: ${seat.row}, Seat: ${seat.number}`, {
+            align: "center",
+          })
+          .moveDown(3);
+      });
+    };
+
     addHeader();
-    addSeats(reservation.seats.slice(i, i + seatsPerPage));
+    addSeats(reservation.seats.slice(0, seatsPerPage));
     addFooter();
-  }
 
-  doc.end();
+    for (
+      let i = seatsPerPage;
+      i < reservation.seats.length;
+      i += seatsPerPage
+    ) {
+      doc.addPage();
+      addHeader();
+      addSeats(reservation.seats.slice(i, i + seatsPerPage));
+      addFooter();
+    }
+
+    doc.end();
+  } catch (error) {
+    console.error(error);
+  }
 };
